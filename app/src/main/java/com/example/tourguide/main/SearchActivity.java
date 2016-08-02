83d217ee301d133adf08fe280c1bfa363eefc74d
Onkar <ganjewaronkar@gmail.com>
2016-08-02 16:20:44 -0700

    filtered search categories

--- app/src/main/java/com/example/tourguide/main/SearchActivity.java
+++ app/src/main/java/com/example/tourguide/main/SearchActivity.java
@@ -1,8 +1,18 @@
 package com.example.tourguide.main;
 
+import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Parcelable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,17 +23,24 @@ import android.widget.TextView;
 
 import com.example.tourguide.business.BusinessModel;
 import com.example.tourguide.yelp.YelpController;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.project.name.R;
 import com.yelp.clientlib.connection.YelpAPI;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.logging.Handler;
 
 /**
  *  sample activity to initiate the search
  */
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements LocationListener {
 
     // controller responsible for carrying out the search
     private YelpController yelpController;
@@ -32,8 +49,19 @@ public class SearchActivity extends AppCompatActivity {
     // UI elements
     private Button btnGo;
 
+    // Variables to get current location
+    protected Location mLastLocation;
+    protected LocationRequest mLocationRequest;
+    protected Marker mCurrLocationMarker;
+
     // progress dialog indicating "wait"
     private ProgressDialog progress;
+
+    private String TAG = "Search_Activity";
+    private String cityName;
+    private String provider;
+    private double _latitude, _longitude;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,16 +73,21 @@ public class SearchActivity extends AppCompatActivity {
 
         // initialize UI elements
         _init();
+
+        getCurrentCity();
+        performSearch();
     }
 
-    private void _init() {
-        // sample button to initiate search
-        btnGo = (Button) findViewById(R.id.btnGo);
+    private void performSearch() {
+
         btnGo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // initiate the asynchronous search on background thread
-                yelpController.performSearch(yelpAPI,getApplicationContext());
+                Log.i(TAG,String.valueOf(_latitude)+""+String.valueOf(_longitude));
+                Log.i(TAG,cityName);
+
+                yelpController.performSearch(yelpAPI,getApplicationContext(),cityName);
 
                 // show progress bar until the action completes
                 progress = new ProgressDialog(v.getContext());
@@ -64,12 +97,78 @@ public class SearchActivity extends AppCompatActivity {
                 progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
                 progress.show();
             }
-        });
+        });}
+
+    private void _init() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Creating an empty criteria object
+        Criteria criteria = new Criteria();
+        // Getting the name of the provider that meets the criteria
+        provider = locationManager.getBestProvider(criteria, false);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        _latitude = location.getLatitude();
+        _longitude= location.getLongitude();
+
+        // sample button to initiate search
+        btnGo = (Button) findViewById(R.id.btnGo);
+
+    }
+
+
+    private void getCurrentCity() {
+        Geocoder gcd = new Geocoder(getApplicationContext(), Locale.getDefault());
+        List<Address> addresses = null;
+        try {
+            addresses = gcd.getFromLocation(_latitude, _longitude, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (addresses.size() > 0)
+            cityName = addresses.get(0).getLocality();
+
+        Log.i(TAG,"Address"+cityName);
+
     }
 
     private void _initController() {
         // start the controller
         yelpController = new YelpController();
         yelpAPI = yelpController.init();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+
+        Log.d(TAG,"Latitude : "+location.getLatitude());
+        Log.d(TAG,"Longitude : "+location.getLongitude());
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
--- app/src/main/java/com/example/tourguide/yelp/YelpController.java
+++ app/src/main/java/com/example/tourguide/yelp/YelpController.java
@@ -2,6 +2,8 @@ package com.example.tourguide.yelp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.util.Log;
 
 import com.example.tourguide.business.BusinessModel;
@@ -15,6 +17,7 @@ import com.yelp.clientlib.entities.SearchResponse;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import retrofit2.Call;
@@ -39,7 +42,7 @@ public class YelpController {
     private YelpAPIFactory apiFactory;
 
     // search limit for nearby attractions
-    private static final int searchLimit = 10;
+    private static final int searchLimit = 20;
 
     // currently hardcoded as "active"
     // but can be filtered as per the user request, in future
@@ -73,27 +76,108 @@ public class YelpController {
     }
 
 
-    public int performSearch(YelpAPI yelpAPI, final Context context) {
-        /*
+    public int performSearch(YelpAPI yelpAPI, final Context context, String currentCity) {
+
         // method to include multiple keywords for search
-        String userKeywords = "";
-        String []someSTringArray = {"active"};
-        for(String eachValue : someSTringArray)
+        String userKeywords = "beaches";
+        String []someSTringArray = {""};
+        String[] filter = {","};
+        String[] temp = {"beaches","skydiving","artmuseums","galleries","lakes","hiking","waterparks","amusementparks"};
+        for(String eachValue : temp)
         {
             userKeywords = userKeywords + "," + eachValue;
-        }*/
+        }
 
         // general params
-        searchParams.put("term", "active");
-        searchParams.put("limit", Integer.toString(searchLimit));
+        searchParams.put("category_filter", "beaches");
+        for (int j =0 ; j<temp.length; j++) {
+            Log.d("DEBUG","Length: "+temp.length);
+            searchParams.clear();
+            searchParams.put("category_filter", temp[j]);
+            Log.d("DEBUG",temp[j]);
+            // locale params
+            searchParams.put("lang", "en");
+            Call<SearchResponse> call = yelpAPI.search(currentCity, searchParams);
+            // asynch http request for search
+            Callback<SearchResponse> callback = new Callback<SearchResponse>() {
+                @Override
+                public void onResponse(Call<SearchResponse> call, Response<SearchResponse> response) {
+                    SearchResponse searchResponse = response.body();
 
-        // locale params
-        searchParams.put("lang", "en");
+                    // local variables to hold the search results
+                    String name, imageUrl ,description, formattedUrl;
+                    int len;
+                    ArrayList<String> address;
+                    Double rating,latitude,longitude;
+
+                    // Update UI text with the searchResponse.
+                    businesses = searchResponse.businesses();
+                    int size = searchResponse.businesses().size();
+                    Log.d("DEBUG","SIZE: "+size);
+                    for (int i=0; i<3; i++) {
+                        name = businesses.get(i).name();
+                        Log.d("DEBUG",name);
+                        if (name.equalsIgnoreCase("Billy Beez"))
+                            continue;
+                        rating = businesses.get(i).rating();
+                        if (businesses.get(i).displayPhone() != "") {
+                            phone = businesses.get(i).phone();
+                        }
+                        latitude = businesses.get(i).location().coordinate().latitude();
+                        longitude = businesses.get(i).location().coordinate().longitude();
+                        imageUrl = businesses.get(i).imageUrl();
+                        boolean flag = businesses.get(i).isClosed();
+//                    businesses.get(1).
+                        if (flag) {
+                            status = "closed";
+                        } else {
+                            status = "open";
+                        }
+                        address = businesses.get(i).location().displayAddress();
+                        description = businesses.get(i).snippetText();
+//                    message = "http://s3-media4.fl.yelpcdn.com/photo/t9sA0-vh8UVUJXAfww_bvQ/ms.jpg";
+                        if (imageUrl!= null) {
+                            len = imageUrl.length();
+                            formattedUrl = imageUrl.substring(0, len - 6) + "o.jpg";
+                            imageUrl = formattedUrl;
+                            Log.i("DEBUG",formattedUrl);
+                        }// not able to implement it yet
+                        int imageResourceId = R.drawable.cafe_theme;
+
+                        // create the model for every location
+                        BusinessModel newObject = new BusinessModel(name,description,status,imageUrl,address,rating,phone,latitude,longitude,imageResourceId);
+                        businessModelList.add(newObject);
+                    }
+                    // start the new activity to display cards
+                    Intent intent = new Intent(context, MainActivity.class);
+                    intent.putParcelableArrayListExtra("MyObj", (ArrayList<BusinessModel>) businessModelList);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(intent);
+                }
+
+                @Override
+                public void onFailure(Call<SearchResponse> call, Throwable t) {
+                    // HTTP error happened, do something to handle it.
+                    // Error
+                    t.printStackTrace();
+                    Log.d("DEBUG","SEARCH FAILED");
+                    return;
+                }
+            };
+            call.enqueue(callback);
+        }
+
+
+//        Log.i("DEBUG",userKeywords);
+//        searchParams.put("limit", Integer.toString(searchLimit));
+//        searchParams.put("terms","arts,active");
+//        searchParams.put("terms",)
+
 
         // currently set the value to San Francisco
         // TODO: update the value as per the current location
-        Call<SearchResponse> call = yelpAPI.search("San Francisco", searchParams);
-
+/*
+        Call<SearchResponse> call = yelpAPI.search(currentCity, searchParams);
         // asynch http request for search
         Callback<SearchResponse> callback = new Callback<SearchResponse>() {
             @Override
@@ -101,14 +185,16 @@ public class YelpController {
                 SearchResponse searchResponse = response.body();
 
                 // local variables to hold the search results
-                String name, imageUrl ,description;
+                String name, imageUrl ,description, formattedUrl;
+                int len;
                 ArrayList<String> address;
                 Double rating,latitude,longitude;
 
                 // Update UI text with the searchResponse.
                 businesses = searchResponse.businesses();
-
-                for (int i=0; i<searchLimit; i++) {
+                int size = searchResponse.businesses().size();
+                Log.d("DEBUG","SIZE: "+size);
+                for (int i=0; i<size; i++) {
                     name = businesses.get(i).name();
                     rating = businesses.get(i).rating();
                     if (businesses.get(i).displayPhone() != "") {
@@ -118,6 +204,7 @@ public class YelpController {
                     longitude = businesses.get(i).location().coordinate().longitude();
                     imageUrl = businesses.get(i).imageUrl();
                     boolean flag = businesses.get(i).isClosed();
+//                    businesses.get(1).
                     if (flag) {
                         status = "closed";
                     } else {
@@ -125,8 +212,13 @@ public class YelpController {
                     }
                     address = businesses.get(i).location().displayAddress();
                     description = businesses.get(i).snippetText();
-
-                    // not able to implement it yet
+//                    message = "http://s3-media4.fl.yelpcdn.com/photo/t9sA0-vh8UVUJXAfww_bvQ/ms.jpg";
+                    if (imageUrl!= null) {
+                        len = imageUrl.length();
+                        formattedUrl = imageUrl.substring(0, len - 6) + "o.jpg";
+                        imageUrl = formattedUrl;
+                        Log.i("DEBUG",formattedUrl);
+                    }// not able to implement it yet
                     int imageResourceId = R.drawable.cafe_theme;
 
                     // create the model for every location
@@ -144,11 +236,12 @@ public class YelpController {
             public void onFailure(Call<SearchResponse> call, Throwable t) {
                 // HTTP error happened, do something to handle it.
                 // Error
+                t.printStackTrace();
             Log.d("DEBUG","SEARCH FAILED");
             return;
             }
         };
-        call.enqueue(callback);
+        call.enqueue(callback);*/
         return 0;
     }
 }
